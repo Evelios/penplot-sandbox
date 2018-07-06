ddeba99c18101415648e0a6425aca40d75998d6d
Thomas Waters <twaters117@gmail.com>
2018-07-06 16:41:41 -0400

    Alchemy Spokes

--- alchemy.js
+++ alchemy.js
@@ -1,6 +1,7 @@
 import newArray from 'new-array';
 import Vector from 'vector';
 import regularPolygon from 'regular-polygon';
+import lineIntersection from 'line-segment-intersection';
 // import SimplexNoise from 'simplex-noise';
 import flattenLineTree from './flatten-line-tree';
 import SimplexNoise from 'simplex-noise';
@@ -19,7 +20,7 @@ export default function createPlot(context, dimensions) {
   const margin = 1;
   const working_width = width - margin;
   const working_height = height - margin;
-  const num_polys = 5;
+  const num_polys = 6;
   const circle_radius = 2;
 
   //---- Generate all the n-gon's ----
@@ -27,25 +28,17 @@ export default function createPlot(context, dimensions) {
     const smallest_poly = 3;
     const poly_sides = smallest_poly + poly_num;
 
-    console.log(`Number of Polygon Sides: ${poly_sides}`);
     const poly_center = [
       margin + circle_radius + poly_num / num_polys * (working_width),
       height / 2
     ];
 
-    console.log(`Polygon Number: ${poly_sides}`);
-
     // Generate a single n-sided polygon
     const polygon_border = regularPolygon(poly_sides, poly_center, circle_radius);
-    console.log(`Polygon Border: ${poly_sides}`);
 
     // Generate the internal polygon design
     const poly_design = generate_spokes(polygon_border);
 
-    // console.log(polygon_border);
-    // return polygon_border;
-    console.log("Finished Designs:")
-    console.log([polygon_border, poly_design]);
     return [polygon_border, poly_design];
   });
 
@@ -53,25 +46,58 @@ export default function createPlot(context, dimensions) {
    * Generate the spokes from polygon vertex towards the center of a
    * regular polygon
    * 
-   * @param {Vector[]} polygon_edges The edges of a regular polygon to create
+   * @param {Vector[]} polygon_verticies The edges of a regular polygon to create
    *  the spokes off of
    * @returns {Vector[]} All the spokes created inside a regular polygon
    */
-  function generate_spokes(polygon_edges) {
-    console.log(`Polygon Edges:`);
-    console.log(polygon_edges);
+  function generate_spokes(polygon_verticies) {
+    const verticies_and_midpoints = polygon_verticies.reduce((acc, curr, index, array) => {
+      const next_point = array[(index + 1) % array.length];
+      return acc.concat([ curr, Vector.midpoint(curr, next_point) ]);
+    }, []);
 
     let alchemy_spokes = [];
-    for (let vertex_count = 0; vertex_count < polygon_edges.length; vertex_count++) {
-      const vertex = polygon_edges[vertex_count];
-      const opposite_point = get_opposite_point(polygon_edges, vertex_count);
-
-      alchemy_spokes.push([
-        polygon_edges[vertex_count],
+    const combined_length = verticies_and_midpoints.length;
+    
+    for (let vertex_index = 0; vertex_index < combined_length; vertex_index += 2) {
+    // for (let vertex_index = 2; vertex_index < 4; vertex_index += 2) {
+      // Generate the spoke line
+      const opposite_index = (vertex_index + combined_length / 2 + 1) % combined_length;
+      const vertex = verticies_and_midpoints[vertex_index];
+      const opposite_point = verticies_and_midpoints[opposite_index];
+      
+      const extended_spoke = [
+        vertex,
         opposite_point
-      ]);
+      ];
+
+      // alchemy_spokes.push(extended_spoke); // For Debug
+
+      // Generate the line that will be the point for the spoke
+      const adjacent_index = (combined_length + vertex_index - 1) % combined_length;
+      const opposite_adj_index = (adjacent_index + combined_length / 2) % combined_length;
+      const adjacent_vertex = verticies_and_midpoints[adjacent_index];
+      const opposite_adj_vertex = verticies_and_midpoints[opposite_adj_index];
+
+      const cutoff_line = [
+        adjacent_vertex,
+        opposite_adj_vertex
+      ];
+
+      // alchemy_spokes.push(cutoff_line); // For Debug
+
+      // Now get the intersecion
+      // ...
+      const intersection = lineIntersection(extended_spoke, cutoff_line);
+
+      // const internal_poly = regularPolygon(20, intersection, 0.2);
+      // internal_poly.push(internal_poly[0]);
+      // alchemy_spokes.push(internal_poly);
+
+      alchemy_spokes.push([vertex, intersection]);
     }
 
+    console.log(alchemy_spokes);
     return alchemy_spokes;
   }
 
@@ -96,7 +122,7 @@ export default function createPlot(context, dimensions) {
       const polygon_edges = poly[0];
       context.beginPath();
       polygon_edges.forEach(p => context.lineTo(p[0], p[1]));
-      context.lineTo(poly[0][0], poly[0][1]);
+      context.lineTo(polygon_edges[0][0], polygon_edges[0][1]);
       context.stroke();
 
       // ---- Polygon Spokes
@@ -115,34 +141,5 @@ export default function createPlot(context, dimensions) {
     return polylinesToSVG(lines, {
       dimensions
     });
-  }
-
-  //---- Helper Functions ----------------------------------------------------
-
-  /**
-   * From a regular polygon, get the opposite point from a particular vertex.
-   * If the polygon has an odd number of sides, this would be the midpoint
-   * of the opposite edge.
-   * @param {Vector[]} regular_polygon 
-   * @param {number} vertex_index 
-   * @returns {Vector} The location opposite a vertex
-   */
-  function get_opposite_point(regular_polygon, vertex_index) {
-    const n_sides = regular_polygon.length;
-
-    // Even number of verticies
-    if (n_sides % 2 == 0) {
-      const opposite_vertex = (n_sides + vertex_index) % n_sides;
-      return regular_polygon[opposite_vertex];
-    }
-
-    // Odd number of verticies
-    else {
-      const half_minus_one = Math.floor(n_sides / 2);
-      return Vector.midpoint(
-        regular_polygon[half_minus_one % n_sides],
-        regular_polygon[(half_minus_one + 1) % n_sides]
-      );
-    }
   }
 }
