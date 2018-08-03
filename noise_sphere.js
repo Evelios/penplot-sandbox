bf535e9119b9360384b49dc5fd6389fa16cfed08
Thomas Waters <twaters117@gmail.com>
2018-08-03 11:13:49 -0400

    Noise Sphere With Worley Noise

--- noise_sphere.js
+++ noise_sphere.js
@@ -2,52 +2,56 @@ import newArray from 'new-array';
 import Vector from 'vector';
 import regularPolygon from 'regular-polygon';
 import SimplexNoise from 'simplex-noise';
+import WorleyNoise from 'worley-noise';
 import { PaperSize, Orientation } from 'penplot';
-import { randomFloat, setSeed } from 'penplot/util/random';
+import { randomFloat, setSeed, randomInt } from 'penplot/util/random';
 import { polylinesToSVG } from 'penplot/util/svg';
 import { clipPolylinesToBox } from 'penplot/util/geom';
-
-setSeed(2);
 
 export const orientation = Orientation.LANDSCAPE;
 export const dimensions = PaperSize.LETTER;
 
 export default function createPlot (context, dimensions) {
   const [ width, height ] = dimensions;
-  const margin = 2.5;
+  const margin = 1;
   const working_width = width - margin;
   const working_height = height - margin;
 
-  const center = [2 * width / 7, 3 * height / 4];
+  const center = [width / 2, height / 2];
   const maxCircleRadius = 7.5;
   const minCircleRadius = 1.5;
   const polygonSides = 300;
-  const numberOfCircles = 200;
+  const numberOfCircles = 150;
   const circleZSpacing = 0.125;
   const noiseStrength = 1.5;
   const noiseScale = 0.15;
   const simplex = new SimplexNoise();
+  const worley = new WorleyNoise(100, randomInt(10000));
+  const worleyScale = Math.max(width, height);
+  const worleyStrength = 10;
 
   let lines = newArray(numberOfCircles).map((_, cirNum) => {
-    const cirPos = Vector.add(center, [
-        1.25 * working_width  / 7 * cirNum / numberOfCircles,
-           -working_height / 4 * cirNum / numberOfCircles
-      ]);
-
-    const circleRadius = minCircleRadius + (maxCircleRadius - minCircleRadius) * cirNum / numberOfCircles;
+    const cirPos = center;
+    const normalizedCirNum = cirNum / numberOfCircles;
+    const radiusAdjustment = Math.sin(Math.PI * normalizedCirNum);
+    const circleRadius = maxCircleRadius * radiusAdjustment;
     let circle = regularPolygon(polygonSides, cirPos, circleRadius);
-    // There is a bug in here somewhere. It could have to do with the
-    // angle function clamping when x=0. There are weird artifacts at
-    // the top and bottom of the circles (and occasionally broken things)
+    circle.push(circle[0]);
 
     // Offset the circle verticies by a noise function
     circle = circle.map((vertex) => {
       const angle = Vector.angle(Vector.subtract(center, vertex));
-      const noiseAmmount = noiseStrength * simplex.noise3D(
+      const simplexAmmount = noiseStrength * simplex.noise3D(
         noiseScale * vertex[0],
         noiseScale * vertex[1],
         noiseScale * cirNum * circleZSpacing
       );
+      const worleyAmmount = worleyStrength * worley.getEuclidean(
+        vertex[0] / worleyScale,
+        vertex[1] / worleyScale,
+        1
+      );
+      const noiseAmmount = 1 * simplexAmmount + 1.5 * worleyAmmount;
       const noiseVector = Vector.Polar(noiseAmmount, angle);
       return Vector.add(vertex, noiseVector);
     });
