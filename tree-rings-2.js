3aaf2ac3a3151f033b43fd19299c82deb57326f1
Thomas Waters <twaters117@gmail.com>
2018-08-29 20:15:33 -0400

    Created initial piece for tree-rings-2

--- tree-rings-2.js
+++ tree-rings-2.js
@@ -9,6 +9,7 @@ import { polylinesToSVG } from 'penplot/util/svg';
 import { clipPolylinesToBox } from 'penplot/util/geom';
 import createStroke from 'penplot-stroke';
 import flattenLineTree from './flatten-line-tree';
+import optimizePath from 'optimize-paths';
 
 export const orientation = Orientation.LANDSCAPE;
 export const dimensions = PaperSize.LETTER;
@@ -23,41 +24,55 @@ export default function createPlot(context, dimensions) {
   const maxCircleRadius = 7.5;
   const minCircleRadius = 0.1;
   const polygonSides = 300;
-  const numberOfCircles = 60;
-  const circleZSpacing = 0;
-  const noiseStrength = 1;
-  const noiseScale = 0.05;
+  const numberOfCircles = 20;
+  const noiseStrength = 2;
+  const stroke_noise_strength = 5;
+  const noiseScale = 0.075;
+  const stroke_noise_scale = 0.075;
   const simplex = new SimplexNoise();
   const pen_thickness = 0.03;
-  const max_line_width = 0.1;
-  const min_line_width = pen_thickness;
+  const line_width = pen_thickness * 15;
+  // const max_line_width = pen_thickness * 5;
+  // const min_line_width = pen_thickness;
 
   let lines = newArray(numberOfCircles).map((_, cirNum) => {
     const cirPos = center;
     const normalizedCirNum = cirNum / numberOfCircles;
     const radiusAdjustment = Math.sin(Math.PI * normalizedCirNum); // For looking more rounded
     const circleRadius = minCircleRadius + maxCircleRadius * radiusAdjustment;
-    let circle = regularPolygon(polygonSides, cirPos, circleRadius);
-    circle.push(circle[0]);
+    const circle = regularPolygon(polygonSides, cirPos, circleRadius, randomFloat(2*Math.PI));
 
-    const line_width =  min_line_width + 
-      ((noiseFunction(Vector.Polar(circleRadius * 20, 0)) + 1) / 2) *
-      (max_line_width - min_line_width);
+    // const line_width_noise = noiseFunction(Vector.Polar(circleRadius * 20, 0));
+    // const norm_line_width_noise = (line_width_noise + 1) / 2;
+    // const line_width =  min_line_width + norm_line_width_noise * (max_line_width - min_line_width);
 
     // Offset the circle verticies by a noise function
-    circle = circle.map((vertex) => {
+    const circle_offset = circle.map((vertex) => {
       const angle = Vector.angle(Vector.subtract(center, vertex));
-      const simplexAmmount = noiseStrength * noiseFunction(vertex);
+      const simplex_ammount = noiseStrength * noiseFunction(vertex);
 
-      const noiseVector = Vector.Polar(simplexAmmount, angle);
-      return Vector.add(vertex, noiseVector);
+      const noise_vector = Vector.Polar(simplex_ammount, angle);
+
+      return Vector.add(vertex, noise_vector);
     });
 
-    // console.log("Before", circle);
-    circle = createStroke(circle, line_width, pen_thickness);
-    // console.log("After", circle);
+    const line_width_style = circle.map(vertex => {
+      const raw_noise = strokeOffsetNoiseFunction(vertex);
+      const min_value = 0.05;
+      const adjusted_noise = raw_noise < min_value && raw_noise > -min_value ?
+        min_value : raw_noise;
+          
+      return stroke_noise_strength * adjusted_noise;
+    });
 
-    return circle;
+    const circle_strokes = createStroke(circle_offset, line_width, pen_thickness, {
+
+      // line_width_style : [5, 4, 3, 2, 1, 0.5, 0.25, 0, 0.25, 0.5, 1, 2, 3, 4],
+      line_width_style : line_width_style,
+      polygon : true,
+    });
+
+    return circle_strokes;
 
   });
 
@@ -91,6 +106,21 @@ export default function createPlot(context, dimensions) {
     }
   }
 
+  function strokeOffsetNoiseFunction(vertex) {
+    return noise(vertex, 1, 1, 5);
+
+    function noise(vertex, strength, frequency, depth) {
+      const recursive_noise = depth > 0 ?
+       noise(vertex, strength / 2, frequency / 2, depth - 1) :
+       0;
+
+      return strength * simplex.noise2D(
+        stroke_noise_scale * (vertex[0] / frequency),
+        stroke_noise_scale * (vertex[1] / frequency)
+      ) + recursive_noise;
+    }
+  }
+
   function draw() {
 
     lines.forEach(circle => {
@@ -102,7 +132,7 @@ export default function createPlot(context, dimensions) {
   }
 
   function print() {
-    return polylinesToSVG(lines, {
+    return polylinesToSVG(optimizePath(lines), {
       dimensions : dimensions,
       lineWidth  : pen_thickness
     });
